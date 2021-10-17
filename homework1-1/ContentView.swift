7003f796d911eb95f60f9b747734384f2e91f966
jojo <jojo22123770@gmail.com>
2021-10-16 21:52:13 -0700

    Initial Commit

---
+++ homework1-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework1-1
+//
+//  Created by jojo on 2021/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homework1-1/homework1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  homework1_1App.swift
+//  homework1-1
+//
+//  Created by jojo on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct homework1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
